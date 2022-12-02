937f2e539ac06e422555ea78df67aa837c0b4e10
ywalakamar <barxwells@gmail.com>
2022-12-02 20:19:31 +0300

    chore: Client Application
    - [ ] Create client application that uses the app

--- src/App.java
+++ src/App.java
@@ -1,6 +1,26 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        EmailApplicaton app = new EmailApplicaton();
-        app.processMessage("Hello Maxwel", "admin@host.com");
+        String message="Hello Maxwel";
+        String email="admin@host.com";
+        String telephone="0722000000";
+        MessageServiceInjector injector=null;
+        Consumer app=null;
+
+
+        /*
+         * Send Email
+         */
+        injector=new EmailServiceInjector();
+        app=injector.getConsumer();
+        app.processMessages(message, email);
+
+
+        /*
+         * Send SMS
+         */
+        injector=new SMSServiceInjector();
+        app=injector.getConsumer();
+        app.processMessages(message, telephone);
+
     }
 }
